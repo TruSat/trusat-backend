3785ea818faa8f61ed14ceb1e2733cfc87d692b8
Kmoneal <kmacjoneal@gmail.com>
2019-08-21 18:47:18 +0000

    Add endpoints
    Introduce new endpoints for and integrate gmail usage. Abstract
    files reading in sensitive information.
    
    * /mostRecent returns most recent observations that were submitted
      to the database
    * /priority returns the object information for what are considered
      priority objects
    * /priority-json is a test endpoint to verify json structure and
      conversion between types and back to a json usable struct
    * /getNonce provides a nonce for a user based on their Ethereum
      address
    * /login takes a signed message form the Ethereum user and
      verifies their identity by checking it against the randomly
      generated nonce
    * /profile is a placeholder with filler information to use while
      structuring the return json
    * /email is an endpoint that will take in the 'to' address and
      a payload. This message will then be sent using the associated
      email address

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import ssl
 from io import BytesIO
 import json
 import secrets
@@ -10,36 +11,40 @@ from eth_account.messages import defunct_hash_message, encode_defunct
 import sha3
 
 import database
+import google_email
 
 PORT_NUMBER = 8080
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        db = database.Database('Astrana.db', 'sqlite', "NULL", "NULL", "NULL")
+        f = open('login.txt', 'r')
+        lines = f.readlines()
+        db_name = lines[0].strip()
+        db_type = lines[1].strip()
+        endpoint = lines[2].strip()
+        username = lines[3].strip()
+        password = lines[4].strip()
+        f.close()
+        db = database.Database(db_name, db_type, endpoint, username, password)
 
         if self.path == "/mostRecent":
             print("Homepage")
             community_recent_query = {}
             community_recent_query['community_observations'] = []
-            recent_observations = db.getRecentObservations()
             try:
                 recent_observations = db.getRecentObservations()
+                print(recent_observations)
             except:
                 print("only latest observation")
             for tup in recent_observations:
                 entry = {
                     "object_name": "Filler",
+                    "object_origin": "Filler",
                     "object_type": "Filler",
-                    "user_name": tup[2],
-                    "time_stamp": datetime.datetime(tup[7], 
-                        tup[8],
-                        tup[9],
-                        tup[10],
-                        tup[11],
-                        tup[12],
-                        tup[13]).timestamp(),
-                    "object_number": tup[0],
-                    "international_designation": tup[4]
+                    "object_purpose": "Filler",
+                    "time_last_tracked": tup[7].decode('utf-8'),
+                    "address_last_tracked": "filler",
+                    "username_last_tracked": tup[2].decode('utf-8')
                     }
                 community_recent_query["community_observations"].append(entry)
             response_body = bytes(json.dumps(community_recent_query), 'utf-8')
@@ -48,6 +53,63 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_header('Access-Control-Allow-Origin', '*')
             self.end_headers()
             self.wfile.write(response_body)
+            
+        elif self.path == "/priority":
+            print("Priority")
+            community_priority_query = {}
+            community_priority_query['community_observations'] = []
+            try:
+                recent_observations = db.selectGlobalPriorities()
+                print(recent_observations)
+            except:
+                print("only latest observation")
+            for tup in recent_observations:
+                entry = {
+                    "object_name": tup[1],
+                    "object_origin": tup[2],
+                    "object_type": tup[3],
+                    "object_purpose": tup[4],
+                    "time_last_tracked": tup[5].timestamp(),
+                    "address_last_tracked": "filler",
+                    "username_last_tracked": tup[6]
+                    }
+                community_priority_query["community_observations"].append(entry)
+            response_body = bytes(json.dumps(community_priority_query), 'utf-8')
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.end_headers()
+            self.wfile.write(response_body)
+
+        elif self.path == "/priority-json":
+            print("Priority-JSON")
+            json_object = db.selectGlobalPriorities_JSON()
+            print(json_object)
+            community_priority_query = {}
+            community_priority_query['community_observations'] = []
+            try:
+                recent_observations = db.selectGlobalPriorities()
+                print(recent_observations)
+            except:
+                print("only latest observation")
+            for tup in recent_observations:
+                entry = {
+                    "object_name": tup[1],
+                    "object_origin": tup[2],
+                    "object_type": tup[3],
+                    "object_purpose": tup[4],
+                    "time_last_tracked": tup[5].timestamp(),
+                    "address_last_tracked": "filler",
+                    "username_last_tracked": tup[6]
+                    }
+                community_priority_query["community_observations"].append(entry)
+            response_body = bytes(json.dumps(community_priority_query), 'utf-8')
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.end_headers()
+            self.wfile.write(response_body)
+        
 
         else:
             self.send_response(200)
@@ -61,7 +123,15 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
         content_length = int(self.headers['Content-Length'])
         body = self.rfile.read(content_length)
-        db = database.Database('Astrana.db')
+        f = open('login.txt', 'r')
+        lines = f.readlines()
+        db_name = lines[0].strip()
+        db_type = lines[1].strip()
+        endpoint = lines[2].strip()
+        username = lines[3].strip()
+        password = lines[4].strip()
+        f.close()
+        db = database.Database(db_name, db_type, endpoint, username, password)
         try:
             db.createTables()
         except:
@@ -75,14 +145,15 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         if self.path == "/getNonce":
             print('getNonce')
             public_address_count = db.getObserverCountByID(public_address=json_body["publicAddress"])
-            random_number = secrets.randbits(64)
+            random_number = secrets.randbits(16)
             response_message = '{"nonce":\"%s\"}' % random_number
-            if public_address_count == 0:
+            print('public address count: ' + str(public_address_count[0]))
+            if public_address_count[0] == None or public_address_count[0] == 0:
                 # New User
                 print("new user")
-                db.addObserver(json_body["publicAddress"], "NULL", "NULL", "NULL")
+                db.addObserver(json_body["publicAddress"], "NULL", 0, "NULL")
                 db.updateObserverNonce(nonce=random_number, public_address=json_body["publicAddress"])
-            if public_address_count == 1:
+            elif public_address_count[0] >= 1:
                 # Old User
                 print("old user")
                 db.updateObserverNonce(nonce=random_number, public_address=json_body["publicAddress"])
@@ -93,13 +164,12 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             print('login')
             old_nonce = db.getObserverNonce(json_body["publicAddress"])
             print(old_nonce[0])
-            nonce = old_nonce[0].encode('utf-8')
+            nonce = str(old_nonce[0]).encode('utf-8')
             message_hash = sha3.keccak_256(nonce).hexdigest()
             message_hash = encode_defunct(hexstr=message_hash)
             print("hashed messaged: " + str(message_hash))
             print(json_body["signedMessage"])
             print("public address: " + json_body["publicAddress"])
-            signed_public_key = Account.recover_message(message_hash, signature=json_body["signedMessage"])
             try:
                 signed_public_key = Account.recover_message(message_hash, signature=json_body["signedMessage"])
                 #signed_public_key = w3.eth.account.recoverHash(str(message_hash), signature=json_body["signedMessage"])
@@ -107,44 +177,68 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             except:
                 print('message could not be checked')
             if signed_public_key.lower() == json_body["publicAddress"].lower():
-                #jwt
-                print("jwt")
                 key = nonce
                 encoded_jwt = jwt.encode({'some':'payload'}, key, algorithm='HS256')
                 db.updateObserverJWT(encoded_jwt, key, json_body["publicAddress"])
-                response_message = b'{"jwt":'
+                response_message = b'{"jwt":"'
                 response_message += encoded_jwt
-                response_message += b'}'
+                response_message += b'"}'
                 response_body = response_message
             else:
                 print("public key is incorrect")
                 response_body = bytes('{}', 'utf-8')
 
-        elif self.path == "/userHomepage":
-            print("Homepage")
-            community_recent_query = {}
-            community_recent_query['community_observations'] = []
-            try:
-                recent_observations = db.getRecentObservations()
-            except:
-                print("only latest observation")
-            for tup in recent_observations:
-                entry = {
+        ### PROFILE ENDPOINT ###
+        elif self.path == "/profile":
+            user_profile = {
+                    "user_name": "Filler_McFiller",
+                    "user_image": "https://i.amz.mshcdn.com/KCJWkZNiwPyNXPcV0CN7yeL8G0A=/fit-in/1200x9600/https%3A%2F%2Fblueprint-api-production.s3.amazonaws.com%2Fuploads%2Fcard%2Fimage%2F784551%2F0e3defde-7d59-4d94-b094-51d187f930da.jpg",
+                    "location": "Filler, FI",
+                    "observation_count": "9001",
+                    "objects_tracked": "1",
+                    "avg_quality_level": "80%",
+                    "bio": "Filler filly fill fill",
+                    "objects_observed": [],
+                    "observation_history": []
+                }
+
+            object_observed = {
                     "object_name": "Filler",
+                    "object_origin": "Filler",
                     "object_type": "Filler",
-                    "user_name": tup[1],
-                    "time_stamp": datetime.datetime(tup[6], 
-                        tup[7],
-                        tup[8],
-                        tup[9],
-                        tup[10],
-                        tup[11],
-                        tup[12]).timestamp(),
-                    "object_number": tup[0],
-                    "international_designation": tup[3]
-                    }
-                community_recent_query["community_observations"].append(entry)
-            response_body = bytes(json.dumps(community_recent_query), 'utf-8')
+                    "object_primary_purpose": "Filler",
+                    "object_secondary_purpose": "Filler",
+                    "observation_count": "0",
+                    "time_last_tracked": "0",
+                    "address_last_tracked": "0x0",
+                    "username_last_tracked": "Filler_McFiller"
+                }
+
+            observation_history = {
+                    "time_submitted": "0",
+                    "object_name": "Filler",
+                    "right_ascension": "Filler",
+                    "declanation": "Filler",
+                    "brightness": "0.0",
+                    "conditions": "Filler"
+                }
+            user_jwt = json_body["jwt"]
+            user_addr = json_body["eth_addr"]
+            credentials = db.getObserverJWT(user_addr)
+            if credentials[0][0] == user_jwt:
+                print(user_jwt)
+
+        elif self.path == '/email':
+            to = json_body['to']
+            message_text = json_body['payload']
+            google_email.send_email(to, message_text)
+
+        else:
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write()
+            return
+
 
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
@@ -152,5 +246,6 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(response_body)
 
-httpd = HTTPServer(('', 8081), SimpleHTTPRequestHandler)
+httpd = HTTPServer(('', 8080), SimpleHTTPRequestHandler)
+#httpd.socket = ssl.wrap_socket(httpd.socket, keyfile='./privkey.pem', certfile='./fullchain.pem', server_side=True)
 httpd.serve_forever()
