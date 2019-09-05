50e6fbe33a776ea6e5b0bc7ea2524d61fafd1533
Kmoneal <kmacjoneal@gmail.com>
2019-09-05 19:57:56 +0000

    Changes to reflect designs and correct errors with endpoints
    * Convert country name to two letter code
    * Minor bug fixes

--- server.py
+++ server.py
@@ -15,6 +15,7 @@ import sha3
 
 import database
 import google_email
+import iod
 
 PORT_NUMBER = 8080
 
@@ -29,62 +30,8 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         password = lines[4].strip()
         f.close()
         db = database.Database(db_name, db_type, endpoint, username, password)
-
-        if self.path == "/mostRecent":
-            print("Homepage")
-            community_recent_query = {}
-            community_recent_query['community_observations'] = []
-            try:
-                recent_observations = db.getRecentObservations()
-                print(recent_observations)
-            except:
-                print("only latest observation")
-            for tup in recent_observations:
-                entry = {
-                    "object_name": "Filler",
-                    "object_origin": "Filler",
-                    "object_type": "Filler",
-                    "object_purpose": "Filler",
-                    "time_last_tracked": tup[7].decode('utf-8'),
-                    "address_last_tracked": "filler",
-                    "username_last_tracked": tup[2].decode('utf-8')
-                    }
-                community_recent_query["community_observations"].append(entry)
-            response_body = bytes(json.dumps(community_recent_query), 'utf-8')
-            self.send_response(200)
-            self.send_header('Content-type', 'application/json')
-            self.send_header('Access-Control-Allow-Origin', '*')
-            self.end_headers()
-            self.wfile.write(response_body)
             
-        elif self.path == "/priority":
-            print("Priority")
-            community_priority_query = {}
-            community_priority_query['community_observations'] = []
-            try:
-                recent_observations = db.selectGlobalPriorities()
-                print(recent_observations)
-            except:
-                print("only latest observation")
-            for tup in recent_observations:
-                entry = {
-                    "object_name": tup[1],
-                    "object_origin": tup[2],
-                    "object_type": tup[3],
-                    "object_purpose": tup[4],
-                    "time_last_tracked": tup[5].timestamp(),
-                    "address_last_tracked": "filler",
-                    "username_last_tracked": tup[6]
-                    }
-                community_priority_query["community_observations"].append(entry)
-            response_body = bytes(json.dumps(community_priority_query), 'utf-8')
-            self.send_response(200)
-            self.send_header('Content-type', 'application/json')
-            self.send_header('Access-Control-Allow-Origin', '*')
-            self.end_headers()
-            self.wfile.write(response_body)
-
-        elif self.path == "/catalog/priorities":
+        if self.path == "/catalog/priorities":
             json_object = db.selectCatalog_Priorities_JSON()
             response_body = bytes(json_object, 'utf-8')
             self.send_response(200)
@@ -252,8 +199,6 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         elif self.path == "/editProfile":
             public_address = json_body["address"]
             user_jwt = json_body["jwt"]
-            print(json_body)
-            print(json_body["email"])
             if db.getObserverJWT(public_address)[0].decode("utf-8") == user_jwt:
                 try:  
                     username = json_body["username"]
@@ -273,6 +218,12 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
                 except Exception as e:
                     print("Bio not being updated")
                     print(e)
+                try:
+                    location = json_body["location"]
+                    db.updateObserverLocation(location, public_address)
+                except Exception as e:
+                    print("Location not being updated")
+                    print(e)
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.send_header('Access-Control-Allow-Origin', '*')
@@ -282,17 +233,19 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
         ### PROFILE ENDPOINT ###
         elif self.path == "/profile":
-            objects_observed_json = json.loads(db.selectObjectsObserved_JSON())
-            observation_history_json = json.loads(db.selectObservationHistory_JSON())
             user_jwt = json_body["jwt"]
             user_addr = json_body["address"]
+            objects_observed_json = db.selectUserObjectsObserved_JSON(user_addr)
+            observation_history_json = db.selectUserObservationHistory_JSON(user_addr)
             credentials = db.getObserverJWT(user_addr)
             user_profile = db.selectProfileInfo_JSON(user_addr)
             user_profile_json = json.loads(user_profile)
             user_profile_json["objects_observed"] = objects_observed_json
             user_profile_json["observation_history"] = observation_history_json
-            if credentials[0].decode("utf-8") == user_jwt:
-                response_body = bytes(json.dumps(user_profile_json), 'utf-8')
+            #if credentials[0].decode("utf-8") == user_jwt:
+            #    response_body = bytes(json.dumps(user_profile_json), 'utf-8')
+            response_body = bytes(json.dumps(user_profile_json), 'utf-8')
+
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.send_header('Access-Control-Allow-Origin', '*')
@@ -311,35 +264,6 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
         elif self.path == '/object/influence':
             norad_number = json_body['norad_number']
-            object_influence = [
-                {
-                    "observation_time": "1550398277",
-                    "username": "Leo Barhorst",
-                    "user_address": "0x1863a72A0244D603Dcd00CeD99b94d517207716a",
-                    "user_location": "Brooklyn, USA",
-                    "observation_quality": "34",
-                    "time_difference": "1.42",
-                    "weight": "33"
-                },
-                {
-                    "observation_time": "1550398277",
-                    "username": "Jim Smith",
-                    "user_address": "0x1863a72A0244D603Dcd00CeD99b94d517207716a",
-                    "user_location": "Los Angeles, USA",
-                    "observation_quality": "45",
-                    "time_difference": "1.42",
-                    "weight": "33"
-                },
-                {
-                    "observation_time": "1550398277",
-                    "username": "Joe Bloggs",
-                    "user_address": "0x1863a72A0244D603Dcd00CeD99b94d517207716a",
-                    "user_location": "London, UK",
-                    "observation_quality": "20",
-                    "time_difference": "1.42",
-                    "weight": "33"
-                }
-                ]
             response_body = bytes(db.selectObjectInfluence_JSON(norad_number), 'utf-8')
             #response_body = bytes(json.dumps(object_influence), 'utf-8')
             self.send_response(200)
@@ -363,10 +287,21 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         elif self.path == '/object/history':
             norad_number = json_body['norad_number']
             year = json_body['year']
-           # object_history = db.selectObjectHistory_summary(norad_number)
-           # response_body = bytes(json.dumps(object_history), 'utf-8')
-            year = json_body["year"]
-            month = json_body["month"]
+            #object_history = db.selectObjectHistory_summary(norad_number)
+            #response_body = bytes(json.dumps(object_history), 'utf-8')
+            year = None
+            month = None
+            try:
+                year = json_body["year"]
+                month = json_body["month"]
+            except Exception as e:
+                print(e)
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.send_header('Access-Control-Allow-Origin', '*')
+                self.end_headers()
+                self.wfile.write(b'[]')
+                return
             real_entry = db.selectObjectHistoryByMonth_JSON(norad_number, year, month)
             real_response = []
             internals = {
@@ -375,15 +310,16 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             }
             new_internals = copy.deepcopy(internals)
             for items in real_entry:
-                items = json.loads(items)
                 date = datetime.fromtimestamp(float(items["observation_time"])).day
                 if date == new_internals["date"]:
+                    items["observation_quality"] = secrets.randbits(7)
                     new_internals["observation"].append(items)
                 else:
                     if new_internals["date"] != 0:
                         real_response.append(new_internals)
                     new_internals = copy.deepcopy(internals)
                     new_internals["date"] = date
+                    items["observation_quality"] = secrets.randbits(7)
                     new_internals["observation"].append(items)
             if new_internals["date"] != 0:
                 real_response.append(new_internals)
@@ -398,30 +334,6 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             norad_number = json_body['norad_number']
             user_jwt = json_body['jwt']
             public_address = json_body['address']
-            object_user_sightings = [
-                    {
-                        "observation_time": "1550398277",
-                        "username": "Leo Barhorst",
-                        "user_location": "Brooklyn, USA",
-                        "observation_quality": "34",
-                        "observation_time_difference": "1.42",
-                        "weight": "10"
-                    },
-                    {
-                        "username": "Leo Barhorst",
-                        "user_location": "Brooklyn, USA",
-                        "observation_quality": "34",
-                        "observation_time_difference": "1.42",
-                        "weight": "1"
-                    },
-                    {
-                        "username": "Leo Barhorst",
-                        "user_location": "Brooklyn, USA",
-                        "quality": "20",
-                        "time_difference": "1.42",
-                        "weight": "0"
-                    }
-                ]
             print(user_jwt)
             tmp = db.selectObjectUserSightings_JSON(norad_number, public_address)
             response_body = bytes(tmp, 'utf-8')
@@ -434,18 +346,22 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         elif self.path == "/tle/object":
             norad_number = json_body["norad_number"]
             two_line_elements = db.selectTLE_single(norad_number)
-            print(two_line_elements)
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.send_header('Access-Control-Allow-Origin', '*')
             self.end_headers()
             try:
-                self.wfile.write(bytes(two_line_elements), 'utf-8')
-            except:
+                self.wfile.write(bytes(two_line_elements, 'utf-8'))
+            except Exception as e:
+                print(e)
                 self.wfile.write(b'')
 
         elif self.path == "/findObject":
             object_name = json_body["objectName"]
+            try:
+                object_name = int(object_name)
+            except Exception as e:
+                print(e)
             objects = db.selectFindObject(object_name)
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
@@ -454,29 +370,41 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             try:
                 self.wfile.write(bytes(objects, 'utf-8'))
             except Exception as e:
-                try:
-                    self.wfile.write(bytes(db.selectFindObject(int(object_name)), 'utf-8'))
-                except:
-                    print('numbers not working either')
-                    self.wfile.write(b'[]')
+                self.wfile.write(b'[]')
                 print(e)
 
         elif self.path == "/submitObservation":
             user_jwt = json_body["jwt"]
+            user_addr = db.getObserverFromJWT(user_jwt)[0]
+            parsed_iod_array = []
+            #credentials = db.getObserverJWT(user_addr)
+            if user_addr == None:
+                self.send_response(401)
+                self.send_header('Access-Control-Allow-Origin', '*')
+                self.end_headers()
+                self.wfile.write(b'')
+                return
             #TODO: FIX THIS, I NEED NEW ADDRESS FLOW NOW!
             try:
                 single = json_body["single"]
+                print(single)
             except Exception as e:
                 print(e)
             try:
                 multiple = json_body["multiple"]
+                for item in multiple:
+                    parsed_iod = iod.parse_iod_lines(item)[0]
+                    if parsed_iod:
+                        parsed_iod_array.append(parsed_iod)
+                db.addParsedIOD(parsed_iod_array, user_addr, datetime.now())
             except Exception as e:
                 print(e)
+            success_length = len(parsed_iod_array)
             self.send_response(200)
-            self.send_header('Content-type', 'text/plain')
+            self.send_header('Content-type', 'application/json')
             self.send_header('Access-Control-Allow-Origin', '*')
             self.end_headers()
-            self.wfile.write(b'')
+            self.wfile.write(bytes(json.dumps(success_length),'utf-8'))
 
         else:
             self.send_response(404)
