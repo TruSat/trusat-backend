80a12e812d3cd5e385fcf63919334eb39f1ca739
Kmoneal <kmacjoneal@gmail.com>
2019-09-13 16:39:37 +0000

    Add additional functions to send claim account email

--- google_email.py
+++ google_email.py
@@ -11,7 +11,7 @@ import base64
 
 
 def create_message(sender, to, subject, message_text):
-    message_text = 'Save this email: TruSat account recovery info for' + to + '\nTo log into TruSat, you\'ll need your password AND this secret code:\n' + message_text + '\nThis email is the only time we can send you this code. Trusat cannot restore your account or reset your password for you. Please save this email forever and make not of the password you used.\nWhy do we do it this way? Read more\nQuestions? Please email: Support@TruSat.org'
+    message_text = 'Save this email: TruSat account recovery info for ' + to + '\nTo log into TruSat, you\'ll need your password AND this secret code:\n' + message_text + '\nThis email is the only time we can send you this code. Trusat cannot restore your account or reset your password for you. Please save this email forever and make not of the password you used.\nWhy do we do it this way? Read more\nQuestions? Please email: Support@TruSat.org'
     message = MIMEText(message_text)
     message['to'] = to
     message['from'] = sender
@@ -49,5 +49,50 @@ def send_email(to, message_text):
     service = build('gmail', 'v1', credentials=creds)
 
     message_to_send = create_message('kenan.oneal@consensys.net', to, 'TruSat - Save this email: Recovery Info', message_text)
+
+    send_message(service, 'me', message_to_send)
+
+
+
+
+
+
+
+
+
+
+
+
+
+def create_recovery_message(sender, to, subject, message_text):
+    message_text = 'Please use the following link to verify your ownership of the following email:\n' + to + '\nIf you did not request recovery of your account please contact us at: Support@TruSat.org\n' + message_text + '\nThis link will expire in 30 minutes.'
+    message = MIMEText(message_text)
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+    return {'raw': base64.urlsafe_b64encode(message.as_string().encode('utf-8'))}
+
+
+def send_recovery_email(to, message_text):
+    SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+
+    creds = None
+
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_console()
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('gmail', 'v1', credentials=creds)
+
+    message_to_send = create_recovery_message('kenan.oneal@consensys.net', to, 'TruSat - Recover Account', message_text)
 
     send_message(service, 'me', message_to_send)
