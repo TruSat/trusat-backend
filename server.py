c6353c49a8e6ea3b92eea9fa89bcf318784dbd76
Neil McLaren <neil.mclaren@consensys.net>
2019-09-19 12:42:57 +0100

    Delete `database.py` from this repo and use the copy in `sathunt-database`.
    I think there's a strong argument for combining some of these repos into one. That's an argument I can have later, but for the record here's roughly how it goes:
    
     - `sathunt-backend` (hereafter "backend") has a dependency on `dathunt-database` (hereafter "database"). We're fine with making breaking changes to that interface, but with the repos being separate we cannot fix consumers atomically in the same commit/PR as we change the interface, so things can break.
     - As soon as things can break, there's a temptation to take a "known good" copy and work with that, only updating periodically. This is apainful way of working that just delays finding bugs. It imposes source code management overhead as well as a risk of deploying something that's broken.
     - With the (forthcoming) addition of snapshot tests to the backend code, it becomes even more important to update tests at the same time as the databse code (or else to have some communications overhead around what test commit corresponds to what database commit).
     - Conceptually, these bits of code are basically all backend code. The "database" is not a database but an interface to one, which IMHO belongs in the backend.

--- server.py
+++ server.py
@@ -15,7 +15,11 @@ import sha3
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.serialization import load_pem_private_key, load_pem_public_key
 
+# Use the database module from our sister repo. We assume it is installed alongside this repo.
+import sys
+sys.path.insert(1,"../sathunt-database")
 import database
+
 import google_email
 import iod
 
