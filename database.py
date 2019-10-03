504ef427e2360bc7994afcec0cd810eb9dddf96d
Kenan O'Neal <kenanoneal@Kenans-MacBook-Pro.local>
2019-10-03 06:16:03 -0700

    Fix spacing and minor bugs

--- database.py
+++ database.py
@@ -1285,7 +1285,7 @@ class Database:
             self.conn.commit()
         return True
 
-        def updateObserverPassword(self, password, public_address):
+    def updateObserverPassword(self, password, public_address):
         if self._dbtype == "sqlite:":
             self.c.execute(self.updateObserverPassword_query, [password, public_address])
             results = self.c.fetchone()
@@ -1294,7 +1294,7 @@ class Database:
             self.conn.commit()
         return True
 
-     def updateObserverAddress(self, new_address, public_address):
+    def updateObserverAddress(self, new_address, public_address):
         if self._dbtype == "sqlite":
             self.c.execute(self.updateObserverAddress_query, [new_address, public_address])
             results = self.c.fetchone()
@@ -1319,7 +1319,7 @@ class Database:
             results = self.c_selectObserver_query.fetchone()
         return results
 
-        def selectObserverAddressFromEmail(self, email):
+    def selectObserverAddressFromEmail(self, email):
         if self._dbtype == "sqlite":
             self.c.execute(self.selectObserverAddressFromEmail_query, [email])
             results = self.c.fetchone()
@@ -1331,7 +1331,7 @@ class Database:
                 return None
         return results
 
-     def selectObserverAddressFromPassword(self, password):
+    def selectObserverAddressFromPassword(self, password):
         if self._dbtype == "sqlite":
             self.c.execute(self.selectObserverAddressFromPassword_query, [password])
             results = self.c.fetchone()
@@ -2143,7 +2143,7 @@ class Database:
         # Get unique list of observed objects for this user
         # TODO: Seems like there's a way to get a count in the query instead of counting the result in python
         query_tmp_num_obj_tracked = """SELECT ParsedIOD.object_number
-            FROM ParsedIOD, Station.station_num
+            FROM ParsedIOD
             JOIN Station ON ParsedIOD.station_number = Station.station_num
             JOIN Observer ON Observer.id = Station.user
             WHERE Observer.eth_addr = '%(ETH_ADDR)s'
@@ -2190,7 +2190,7 @@ class Database:
             print(e)
             user_first_observation = ''
 
-         query_tmp_email = """SELECT Observer_email.email
+        query_tmp_email = """SELECT Observer_email.email
             FROM Observer_email
             INNER JOIN Observer
             ON Observer_email.user_id=Observer.id
@@ -2600,7 +2600,7 @@ class Database:
         quality = 99 # !TODO
 
         # Get user-related info first
-        query_tmp_count = """SELECT COUNT(Observer.id), Observer.eth_addr
+        query_tmp_count = """SELECT COUNT(Observer.id), Observer.eth_addr, Observer.name
             FROM ParsedIOD
             JOIN Station ON ParsedIOD.station_number = Station.station_num
             JOIN Observer ON Observer.id = Station.user
@@ -2608,7 +2608,7 @@ class Database:
             GROUP BY Observer.id
             ORDER BY ParsedIOD.obs_time DESC
             LIMIT 1;"""
-        query_parameters = {'NORAD_NUMBER': norad_num}
+        query_parameters = {'NORAD_NUM': norad_num}
         self.c.execute(query_tmp_count, query_parameters)
         try:
             (user_count, eth_addr, name) = self.c.fetchone()
@@ -2616,12 +2616,12 @@ class Database:
             print(e)
             return None
 
-         query_tmp_tracked = """SELECT date_format(ParsedIOD.obs_time, '%M %d, %Y')
+        query_tmp_tracked = """SELECT date_format(ParsedIOD.obs_time, '%M %d, %Y')
             FROM ParsedIOD
             WHERE ParsedIOD.object_number = %(NORAD_NUM)s
             ORDER BY ParsedIOD.obs_time DESC
             LIMIT 1;"""
-        query_parameters = {'NORAD_NUMBER': norad_num}
+        query_parameters = {'NORAD_NUM': norad_num}
         self.c.execute(query_tmp_tracked, query_parameters)
         try:
             last_tracked = self.c.fetchone()[0]
