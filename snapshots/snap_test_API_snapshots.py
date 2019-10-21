3cde41d670fe9f0e64968b068567d18317368130
Kmoneal <kmacjoneal@gmail.com>
2019-10-21 17:13:17 -0400

    Initialize snapshots

---
+++ snapshots/snap_test_API_snapshots.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+
+snapshots = Snapshot()
+
+snapshots['test_catalog_priorities 1'] = ''''''
+
+snapshots['test_catalog_undisclosed 1'] = ''''''
+
+snapshots['test_catalog_debris 1'] = ''''''
+
+snapshots['test_catalog_latest 1'] = ''''''
+
+snapshots['test_catalog_all 1'] = ''''''
+
+snapshots['test_tle_trusat_all 1'] = ''''''
+
+snapshots['test_tle_trusat_priorities 1'] = ''''''
+
+snapshots['test_tle_trusat_high_confidence 1'] = ''''''
+
+snapshots['test_astriagraph 1'] = ''''''
+
+snapshots['test_object_influence 1'] = ''''''
+
+snapshots['test_tle_object 1'] = ''''''
+
+snapshots['test_object_info 1'] = ''''''
